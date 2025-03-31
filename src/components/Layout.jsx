f59492425ab7ca8e8bf8bb16016f0bc11681eef6
nbyapp <nishesh@buildyourapp.ai>
2025-03-30 21:19:26 -0400

    Simplify Layout by removing API Settings button

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react';
 import { Outlet, Link, useLocation } from 'react-router-dom';
-import ApiSettings from './ApiSettings';
 
 function Layout() {
   const location = useLocation();
-  const [isApiSettingsOpen, setIsApiSettingsOpen] = useState(false);
   
   const isActive = (path) => {
     return location.pathname === path ? 'bg-primary-100 text-primary-800' : 'text-gray-600 hover:bg-gray-100';
@@ -31,17 +28,7 @@ function Layout() {
                 </Link>
               </nav>
             </div>
-            <div className="flex items-center space-x-4">
-              <button
-                onClick={() => setIsApiSettingsOpen(true)}
-                className="text-gray-500 hover:text-gray-700 flex items-center"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
-                  <path fillRule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd" />
-                </svg>
-                API Keys
-              </button>
-              
+            <div className="flex items-center">
               <a 
                 href="https://github.com/nbyapp/nbyapp" 
                 target="_blank" 
@@ -69,9 +56,6 @@ function Layout() {
           </p>
         </div>
       </footer>
-      
-      {/* API Settings Modal */}
-      <ApiSettings isOpen={isApiSettingsOpen} onClose={() => setIsApiSettingsOpen(false)} />
     </div>
   );
 }
